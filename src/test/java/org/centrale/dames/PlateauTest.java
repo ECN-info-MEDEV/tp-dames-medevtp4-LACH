0502de04624719a6fa2cd1e0210486fd745ed08a
Arthur200111 <arthur.huet01@gmail.com>
2023-01-05 11:25:22 +0100

    Update PlateauTest.java

--- src/test/java/org/centrale/dames/PlateauTest.java
+++ src/test/java/org/centrale/dames/PlateauTest.java
@@ -1,16 +1,27 @@
 package org.centrale.dames;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 
 
 public class PlateauTest {
-    Plateau p;
+    Plateau p = new Plateau();
 
     @Test
     public void testInitPlateau() {
         p = new Plateau();
         
 
+    }
+
+    @Test
+    void testGetCase() {
+        assertEquals(p.getCase("A0").getPion(), null);
+        assertTrue(p.getCase("A1").getPion().getCouleur());
+        assertFalse(p.getCase("A9").getPion().getCouleur());
     }
 }
